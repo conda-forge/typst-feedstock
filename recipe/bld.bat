75ca4d9ff93499a3f4f36fffe924af523a45f572
Adrian Freund <adrian@freund.io>
2024-03-10 18:02:48 +0100

    Add --no-track to cargo invocation
    This prevents cargo from adding .crates.toml and .crates2.json files in
    the install root.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,7 +2,7 @@ cargo-bundle-licenses ^
     --format yaml ^
     --output THIRDPARTY_LICENSES.yaml || goto :error
 
-cargo install --locked --root "%PREFIX%" --path .\crates\typst-cli || goto :error
+cargo install --no-track --locked --root "%PREFIX%" --path .\crates\typst-cli || goto :error
 
 goto :EOF
 
