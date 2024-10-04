b7b64b123c8481661b5d323166fe72e710e36e37
Adrian Freund <adrian@freund.io>
2024-10-04 17:32:21 +0200

    Add activate scripts
    This sets the typst package path to be inside the conda environment.
    Note that typst currently only supports one package path, so setting
    this will disable the default package path at
    ~/.local/share/typst/packages.
    
    If you install typst using `pixi global install` you might want to pass
    `--no-activation` to keep the global package path.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,6 +4,17 @@ cargo-bundle-licenses ^
 
 cargo install --no-track --locked --root "%PREFIX%" --path .\crates\typst-cli || goto :error
 
+setlocal EnableDelayedExpansion
+
+:: Copy the [de]activate scripts to %PREFIX%\etc\conda\[de]activate.d.
+:: This will allow them to be run on environment activation.
+for %%F in (activate deactivate) DO (
+    if not exist %PREFIX%\etc\conda\%%F.d mkdir %PREFIX%\etc\conda\%%F.d
+    copy %RECIPE_DIR%\%%F.bat %PREFIX%\etc\conda\%%F.d\%PKG_NAME%_%%F.bat || goto :error
+    :: Copy unix shell activation scripts, needed by Windows Bash users
+    copy %RECIPE_DIR%\%%F-win.sh %PREFIX%\etc\conda\%%F.d\%PKG_NAME%_%%F.sh || goto :error
+)
+
 goto :EOF
 
 :error
